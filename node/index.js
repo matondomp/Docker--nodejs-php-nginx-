70b282f8a5a1809a11efbc20ac01e80de06b7d26
matondo antonio <matondo.antonio@ideiasdinamicas.com>
2022-11-27 17:10:36 +0100

    added dockerize to aplication docker-compose

--- node/index.js
+++ node/index.js
@@ -1,6 +1,21 @@
 const express = require("express")
+const mysql = require('mysql')
+
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb",
+
+}
+
+const connection = mysql.createConnection(config)
+const sql = `INSERT INTO peaple (name) values('Matondo Pedro')`
+connection.query(sql)
+connection.end()
 
 const app = express()
+
 
 app.get('/', (req, res) => {
     res.send('<div> FULL CYCLE </div>')
